905e9808481731a758a975d407b3c8c2e4bb0331
ppidcard <tvxqlikei@live.cn>
2021-07-12 06:05:10 +1000

    Done

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,25 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser 
+    def initialize(emails)
+        @emails = emails
+    end
+    attr_accessor :emails
+    
+    def parse
+        res = []
+        res << emails.split(' ')
+       ee =  res.flatten.map do |val|
+            if val[val.length - 1] == ','
+                val.delete(val[val.length - 1])
+            else val
+            end
+        end
+        ee.uniq 
+    end
+end
+
+emails = EmailAddressParser.new("john@doe.com, person@somewhere.org, person@somewhere.org ppdicared@www aa aa")
+p emails.parse
